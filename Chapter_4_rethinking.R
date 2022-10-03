ff9d06395aee9cce61a7faec6a751349412f54c2
Megan <megan.verma@gmail.com>
2022-10-03 12:04:02 +0100

    polynomial ex

--- Chapter_4_rethinking.R
+++ Chapter_4_rethinking.R
@@ -1,5 +1,13 @@
 # Chapter 4: Geocentric models in R and Stan
 
+# install packages-----
+
+install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+# we recommend running this is a fresh R session or restarting your current session
+install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+install.packages(c("coda","mvtnorm","devtools","loo","dagitty","shape"))
+devtools::install_github("rmcelreath/rethinking")
+
 # linear regression---- 
 
 library(rethinking)
@@ -275,3 +283,84 @@ lines(weight.seq , mu.mean)
 shade(mu.PI , weight.seq)
 # draw PI region for simulated heights
 shade(height.PI , weight.seq)
+
+# curves from lines------
+
+# polynomials -----
+
+
+plot(d$height ~ d$weight)
+
+# build the square of weight.s as a separate variable
+
+d$weight_s <- (d$weight - mean(d$weight)) / sd(d$weight)
+d$weight_s2 <- d$weight_s ^ 2
+
+# quadratic model
+m4.5 <- quap(
+  alist(
+    height ~ dnorm(mu , sigma) ,
+    mu <- a + b1 * weight_s + b2 * weight_s2 ,
+    a ~ dnorm(178 , 20) ,
+    b1 ~ dlnorm(0 , 1) ,
+    b2 ~ dnorm(0 , 1) ,
+    sigma ~ dunif(0 , 50)
+  ) ,
+  data = d
+)
+
+# model summary 
+precis(m4.5)
+
+# must plot to understand 
+
+weight.seq <- seq(from = -2.2 ,
+                  to = 2 ,
+                  length.out = 30) 
+pred_dat <- list(weight_s = weight.seq , weight_s2 = weight.seq ^ 2)
+mu <- link(m4.5 , data = pred_dat)
+mu.mean <- apply(mu , 2 , mean)
+mu.PI <- apply(mu , 2 , PI , prob = 0.89)
+sim.height <- sim(m4.5 , data = pred_dat)
+height.PI <- apply(sim.height , 2 , PI , prob = 0.89)
+
+plot(height ~ weight_s , d , col = col.alpha(rangi2, 0.5))
+lines(weight.seq , mu.mean)
+shade(mu.PI , weight.seq)
+shade(height.PI , weight.seq)
+
+# higher order -- cubic regression on weight
+
+d$weight_s3 <- d$weight_s ^ 3
+
+m4.6 <- quap(
+  alist(
+    height ~ dnorm(mu , sigma) ,
+    mu <- a + b1 * weight_s + b2 * weight_s2 + b3 * weight_s3 ,
+    a ~ dnorm(178 , 20) ,
+    b1 ~ dlnorm(0 , 1) ,
+    b2 ~ dnorm(0 , 10) ,
+    b3 ~ dnorm(0 , 10) ,
+    sigma ~ dunif(0 , 50)
+  ) ,
+  data = d
+)
+
+# plot cubic linear function 
+weight.seq <- seq(from = -2.2 ,
+                  to = 2 ,
+                  length.out = 30) 
+pred_dat <- list(weight_s = weight.seq , weight_s2 = weight.seq ^ 2,  weight_s3 = weight.seq ^ 3)
+mu <- link(m4.6 , data = pred_dat)
+mu.mean <- apply(mu , 2 , mean)
+mu.PI <- apply(mu , 2 , PI , prob = 0.89)
+sim.height <- sim(m4.6 , data = pred_dat)
+height.PI <- apply(sim.height , 2 , PI , prob = 0.89)
+
+plot(height ~ weight_s , d , col = col.alpha(rangi2, 0.5))
+lines(weight.seq , mu.mean)
+shade(mu.PI , weight.seq)
+shade(height.PI , weight.seq)
+
+# B-splines-----
+
