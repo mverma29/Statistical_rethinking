bbc1aa146334676242641b21342cfbbafd0ba737
Megan Verma <megan.verma@gmail.com>
2022-10-04 14:26:04 +0100

    categorical variables

--- Chapter_5_rethinking.R
+++ Chapter_5_rethinking.R
@@ -483,3 +483,41 @@ shade(mu_PI , xseq)
 
 
 
+
+
+# categorical data----
+# binary categories 
+data(Howell1)
+d <- Howell1
+str(d)
+
+# prior dist for females and males: 
+mu_female <- rnorm(1e4,178,20)
+mu_male <- rnorm(1e4,178,20) + rnorm(1e4,0,10)
+precis( data.frame( mu_female , mu_male ) )
+
+# index variable
+d$sex <- ifelse(d$male == 1 , 2 , 1)
+str(d$sex)
+
+# approximate posterior using index variable
+m5.8 <- quap(alist(
+  height ~ dnorm(mu , sigma) ,
+  mu <- a[sex] ,
+  a[sex] ~ dnorm(178 , 20) ,
+  sigma ~ dunif(0 , 50)
+) , data = d)
+
+precis(m5.8 , depth = 2) # depth tells it to show any vector parameters 
+# output mean is average height for each category! 
+
+# what is expected diff in height between males and females? 
+# sample posterior into a data frame & insert calc into frame
+post <- extract.samples(m5.8)
+post$diff_fm <- post$a[, 1] - post$a[, 2]
+precis(post , depth = 2)
+# calc is called a contrast
+# we have post dist of the difference! 
+
+
+
